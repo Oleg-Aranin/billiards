711f1f47c33e761f3e4fde77130c0b156d60a129
Oleg-A <jsprojectcase@gmail.com>
2020-01-09 02:56:52 +0300

    Updates

---
+++ precache-manifest.396df20e47500e0aa346b6472cd60842.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba4cd6f0348967f8f3c3a4af2646d03b",
+    "url": "/billiards/index.html"
+  },
+  {
+    "revision": "b6ce77c6609b69594958",
+    "url": "/billiards/static/css/main.317cc683.chunk.css"
+  },
+  {
+    "revision": "bb44cc0db4bd6558e275",
+    "url": "/billiards/static/js/2.e6235968.chunk.js"
+  },
+  {
+    "revision": "a8a3f5dfa8738bacaadb90fda48a26d0",
+    "url": "/billiards/static/js/2.e6235968.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6ce77c6609b69594958",
+    "url": "/billiards/static/js/main.cf97bd43.chunk.js"
+  },
+  {
+    "revision": "c32a5d6eebdfec33063b",
+    "url": "/billiards/static/js/runtime-main.4d0f1ade.js"
+  },
+  {
+    "revision": "edcc46e8afb36e2a1e17e90a7bbc4bad",
+    "url": "/billiards/static/media/monster-marbles-sprite-sheets.edcc46e8.jpg"
+  }
+]);
